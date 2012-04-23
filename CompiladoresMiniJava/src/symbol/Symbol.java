c4f377046af94d1604c8568947dfab58e01ed3f6
vladimmir <vladimmir@cb84e2c3-6361-e381-7b3c-ca6f1ed2ccc1>
2012-04-23 20:15:22 +0000

    Adicionando o começo das tabelas( apenas a classe abstrata table) e a classe Symbol.

---
+++ CompiladoresMiniJava/src/symbol/Symbol.java
@@ -0,0 +1,30 @@
+package symbol;
+
+import java.util.Hashtable;
+
+public class Symbol {
+	private String name;
+	private static Hashtable<String,Symbol> dict = new Hashtable<String,Symbol>();
+	private Symbol(String n) {
+		name=n; 
+	}
+	
+	public String toString() {
+		return name;
+	}
+	public static Symbol symbol(String n) {
+		String u = n.intern();
+		Symbol s = (Symbol)dict.get(u);
+		if (s==null) {
+			s = new Symbol(u);
+			dict.put(u,s); 
+		}
+		return s;
+	}
+	
+	public boolean equals(Object s)
+	{
+		return this.name.equals(s.toString());
+	}
+}
+
